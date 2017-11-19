a2f4b018864852059b2a99e6f48428aae6bc1a58
florin141 <29834996+florin141@users.noreply.github.com>
2017-11-20 00:15:49 +0200

    Using Object.create() to Create JavaScript Objects.

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 'use strict';
 
-// constructor functions
-function Cat(name, color) {
-    this.name = name;
-    this.color = color;
-}
-
-var cat = new Cat('Fluffy', 'White');
+var cat = Object.create(Object.prototype, {
+    name: {
+        value: 'Fluffy',
+        enumerable: true,
+        writable: true,
+        configurable: true
+    },
+    color: {
+        value: 'White',
+        enumerable: true,
+        writable: true,
+        configurable: true
+    }
+});
 
 display(cat);
