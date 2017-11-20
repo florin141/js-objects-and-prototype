df73d7002a23dcd4dbfe5d8081b5cc3af3c8bcb4
florin141 <29834996+florin141@users.noreply.github.com>
2017-11-20 21:26:23 +0200

    Using the Writable Attribute.

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 'use strict';
 
 var cat = {
-    name: 'Fluffy',
+    name: {
+        first: 'Fluffy',
+        last: 'LaBeouf'
+    },
     color: 'White'
 };
 
-
-display(Object.getOwnPropertyDescriptor(cat, 'name'));
+Object.defineProperty(cat, 'name', {writable: false});
+Object.freeze(cat.name);
+cat.name.first = 'Scratchy';
+display(cat.name);
