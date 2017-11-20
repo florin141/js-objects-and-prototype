6dddc91ef0c6c2c9adac4ddc38cfce57eb96eff6
florin141 <29834996+florin141@users.noreply.github.com>
2017-11-20 23:27:30 +0200

    Creating Your Own Prototypal Inheritance Chains.

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 'use strict';
 
-// var foo = function () {
-//
-// };
-// display(foo.prototype); // empty object
-//
-// var cat = {
-//     name: 'Fluffy'
-// };
-// display(cat.prototype); // undefined
-// display(cat.__proto__); // empty object
-//
+function Animal(voice) {
+    this.voice = voice || 'grunt';
+}
+
+Animal.prototype.speak = function () {
+    display(this.voice);
+};
 
 function Cat(name, color) {
+    Animal.call(this, 'Meow');                      /// creating the prototype chain
     this.name = name;
     this.color = color;
 }
-
-Cat.prototype.age = 4;
+Cat.prototype = Object.create(Animal.prototype);    /// creating the prototype chain
+Cat.prototype.constructor = Cat;                    /// creating the prototype chain
 
 var fluffy = new Cat('Fluffy', 'White');
 
+fluffy.speak();
+
+display(fluffy);
+display(fluffy instanceof Cat);
+display(fluffy instanceof Animal);
+
 display(fluffy.__proto__);
 display(fluffy.__proto__.__proto__);
-display(fluffy.__proto__.__proto__.__proto__);
